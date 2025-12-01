801c8439bfea8fb4a2d9276c66bb032d48f7a76a
birunthajega29 <birunthajega29@gmail.com>
2025-12-01 15:08:36 +0530

    first commit

--- demo.py
+++ demo.py
@@ -1,5 +1,7 @@
-def func(self):
-    print("hefkjwbk")
-    return "this is my jenkins"
-
-print(func)
+def generator():
+    for i in range(1,6):
+        yield i
+gen = generator()
+# print(next(gen))
+for i in gen:
+    print(i)
