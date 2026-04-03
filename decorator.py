932d2053d9e6776f03b2507dac2c8632238e746c
birunthajega29 <birunthajega29@gmail.com>
2026-04-03 17:34:23 +0530

    extra line added

--- decorator.py
+++ decorator.py
@@ -2,7 +2,8 @@ def decorator(func):
     def inner(*args,**kwargs):
         print("before function run")
         result = func(*args,**kwargs)
-        print("after function run")    return inner
+        print("after function run")
+    return inner
 
 @decorator
 def simple():
